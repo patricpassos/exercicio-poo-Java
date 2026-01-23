5447042b60a6ca860960bb7488e1acb4462dff91
patricpassos <patrickpassos93@gmail.com>
2026-01-23 10:59:18 -0300

    Execicio triangulo sem poo

---
+++ triangulo/application/Program.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the measures of triangle X:");
+		double xA = sc.nextDouble();
+		double xB = sc.nextDouble();
+		double xC = sc.nextDouble();
+
+		System.out.println("Enter the measures of triangle Y:");
+		double yA = sc.nextDouble();
+		double yB = sc.nextDouble();
+		double yC = sc.nextDouble();
+
+		double p = (xA + xB + xC) / 2.0;
+		double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+		p = (yA + yB + yC) / 2.0;
+		double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+		System.out.printf("Triangle X area: %.4f\n", areaX);
+		System.out.printf("Triangle Y area: %.4f\n", areaY);
+
+		if (areaX > areaY) {
+			System.out.println("Large area: X");
+		} else {
+			System.out.println("Large area: Y");
+		}
+		sc.close();
+
+	}
+
+}
