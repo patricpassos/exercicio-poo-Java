543a4b2b312dd06e0bcd88e4ec1d528af5752a72
patricpassos <patrickpassos93@gmail.com>
2026-01-23 11:43:52 -0300

    aplicando classe com três atributos

--- triangulo/application/Program.java
+++ triangulo/application/Program.java
@@ -3,28 +3,34 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Triangle;
+
 public class Program {
 
 	public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		 
+		Triangle x, y;       //variavel do tipo Triangle (Ctrl + shift + o) **memoria stack
+		x = new Triangle(); //instanciação(criação do objeto x) **memoria heap
+		y = new Triangle(); //instanciação(criação do objeto y) **memoria heap
 
 		System.out.println("Enter the measures of triangle X:");
-		double xA = sc.nextDouble();
-		double xB = sc.nextDouble();
-		double xC = sc.nextDouble();
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
 
 		System.out.println("Enter the measures of triangle Y:");
-		double yA = sc.nextDouble();
-		double yB = sc.nextDouble();
-		double yC = sc.nextDouble();
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
 
-		double p = (xA + xB + xC) / 2.0;
-		double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+		double p = (x.a + x.b + x.c) / 2.0;    //**memoria stack
+		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));   //**memoria stack
 
-		p = (yA + yB + yC) / 2.0;
-		double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+		p = (y.a + y.b + y.c) / 2.0;          //**memoria stack
+		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));   //**memoria stack
 
 		System.out.printf("Triangle X area: %.4f\n", areaX);
 		System.out.printf("Triangle Y area: %.4f\n", areaY);
---
+++ triangulo/entities/Triangle.java
@@ -0,0 +1,8 @@
+package entities;
+
+public class Triangle {
+	//declaração dos atributos
+	public double a;
+	public double b;
+	public double c;
+}
