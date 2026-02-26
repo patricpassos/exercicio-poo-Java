b78001a3d6e43476e744565674034f3621c6174e
patricpassos <patrickpassos93@gmail.com>
2026-02-26 12:42:17 -0300

    Exercise completed

--- salary_increase_list/application/ProgramSalary.java
+++ salary_increase_list/application/ProgramSalary.java
@@ -1,9 +1,62 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
 public class ProgramSalary {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Employee> list = new ArrayList<>();
+		 
+		
+		System.out.print("How many employees will be registered? ");
+		int n = sc.nextInt();
+		
+		for(int i = 0; i < n; i++) {
+			System.out.printf("\nEmployee #%d:", i + 1);
+			System.out.print("\nId: ");
+			Integer id = sc.nextInt();
+			
+			while(Employee.hasId(list, id)) {
+				System.out.print("Id already taken. Try again: ");
+				id = sc.nextInt();
+			}
+			
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Salary: ");
+			Double salary = sc.nextDouble();
+			Employee employee = new Employee(id, name, salary);
+			list.add(employee);
+		}
+		
+		System.out.print("\nEnter the employee ID that will have salary increase: ");
+		int idEmployee = sc.nextInt();
+		Integer idPosition = Employee.idPosition(list, idEmployee);
+		
+		if(idPosition == null) {
+			System.out.print("This ID does not exist!");	
+		} else {
+			System.out.print("Enter the percentage: ");
+			double percent = sc.nextDouble();
+			list.get(idPosition).increaseSalary(percent);
+		}
+		
+		System.out.println("\nList of employees:");
+		for (Employee listEmployee : list) {
+			System.out.println(listEmployee);
+		}
+		
+		sc.close();
 
 	}
 
--- salary_increase_list/entities/Employee.java
+++ salary_increase_list/entities/Employee.java
@@ -1,5 +1,66 @@
 package entities;
 
+import java.util.List;
+
 public class Employee {
+	
+	private Integer id;
+	private String name;
+	private Double salary;
+	
+	public Employee(Integer id, String name, Double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	
+	public static boolean hasId(List<Employee> list, int id) {
+		Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		return emp != null;
+	}
+	
+	public static Integer idPosition(List<Employee> list, int idEmployee) {
+		for(int i = 0; i < list.size(); i++) {
+			if(list.get(i).getId() == idEmployee) {
+				return i;
+			}
+		}
+		return null;
+	}
+	
+	public void increaseSalary(double percentage) {
+		salary += salary * percentage / 100.0;
+	}
+	
+	public String toString() {
+		return
+				id + ", "
+				+ name + ", "
+				+ String.format("%.2f", salary);
+	}
 
 }
