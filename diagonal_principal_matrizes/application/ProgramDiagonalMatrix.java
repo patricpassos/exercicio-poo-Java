a188586983604af90e2b7e3fde7f6edebb2ae9ab
patricpassos <patrickpassos93@gmail.com>
2026-03-02 11:51:30 -0300

    exercicio de exemplo matrizes com OO

--- diagonal_principal_matrizes/application/ProgramDiagonalMatrix.java
+++ diagonal_principal_matrizes/application/ProgramDiagonalMatrix.java
@@ -4,33 +4,35 @@ import java.util.Scanner;
 
 import entities.Matriz;
 
-
 public class ProgramDiagonalMatrix {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("What is the order of the matrix? ");
 		int n = sc.nextInt();
-		
-		Matriz matriz = new Matriz(n);
-		
+
+		Matriz matriz = new Matriz(n); // cria um ponteiro na stack que aponta que aponta para o heap (n)
+
 		System.out.println("Digite os valores da matriz:");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-            	System.out.printf("Elemento [%d %d]: ", i, j);
-            	int elemento = sc.nextInt();
-                matriz.setValor(i, j, elemento); // ponte com as referencias de coluna e linha para inputs da matriz
-            }
-        }
-		
-        System.out.println("\nDiagonal principal:");
-        int[] diagonal = matriz.getDiagonalPrincipal(); // ponteiro que aponta para o a "caixa" vetor do metodo
-        for (int valor : diagonal) {
-            System.out.print(valor + " ");
-        }
-        
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.printf("Elemento [%d %d]: ", i, j);
+				int elemento = sc.nextInt();
+				matriz.setValor(i, j, elemento); // ponte com as referencias de coluna e linha para inputs da matriz
+			}
+		}
+
+		System.out.println("\nDiagonal principal:");
+		int[] diagonal = matriz.getDiagonalPrincipal(); // ponteiro que aponta para o a "caixa" vetor do metodo
+		for (int valor : diagonal) {
+			System.out.print(valor + " ");
+		}
+
+		int negativos = matriz.contagemNegativos();
+		System.out.println("\n\nQuantidade de valores negativos: " + negativos);
+
 		sc.close();
 
 	}
--- diagonal_principal_matrizes/entities/Matriz.java
+++ diagonal_principal_matrizes/entities/Matriz.java
@@ -1,13 +1,17 @@
 package entities;
 
 public class Matriz {
-	
-	private int n; //define a ordem da matriz
-	private int [][] dados; // define a estrutura da matriz (matriz)
-	
+
+	private int n; // define a ordem da matriz
+	private int[][] dados; // define a estrutura da matriz (matriz)
+
 	public Matriz(int n) {
-		this.n = n; 
-		this.dados = new int[n][n]; 
+		this.n = n;
+		this.dados = new int[n][n];
+		// realiza uma alocação dinamica de memoria, criando uma estrutura da matriz no
+		// heap
+		// deixa de ser null a passa a guardar o endereço ed memoria. onde os dados
+		// serão armazenados
 	}
 
 	public int getN() {
@@ -18,18 +22,29 @@ public class Matriz {
 		this.n = n;
 	}
 
-	public void setValor(int linha, int coluna, int elemento) { //metodo para insetir os valores da matriz
-        dados[linha][coluna] = elemento;
-    }
-	
-	 public int[] getDiagonalPrincipal() { 
-		 	//intanciado um vetor no heap temporario "RECIPIENTE" com o objetivo de de guarda-los
-	        int[] diagonal = new int[n]; 
-	        for (int i = 0; i < n; i++) {
-	            diagonal[i] = dados[i][i]; //diagonal recebe os valores da diagonal principal do objeto
-	        }
-	        return diagonal; //envia o endereço de memoria do vetor
-	    }
-	
-	
+	public void setValor(int linha, int coluna, int elemento) { // metodo para inserir os valores da matriz
+		dados[linha][coluna] = elemento;
+	}
+
+	public int[] getDiagonalPrincipal() {
+		// intanciado de um novo objeto vetor no heap temporario "RECIPIENTE" com o
+		// objetivo de de guarda-los
+		int[] diagonal = new int[n];
+		for (int i = 0; i < n; i++) {
+			diagonal[i] = dados[i][i]; // diagonal recebe os valores da diagonal principal do objeto
+		}
+		return diagonal; // envia o endereço de memoria do vetor
+	}
+
+	public int contagemNegativos() {
+		int cont = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (dados[i][j] < 0) {
+					cont++;
+				}
+			}
+		}
+		return cont;
+	}
 }
