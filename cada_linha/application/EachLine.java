5d7ff36c2175139206bb2f0736d91c9379839ccf
patricpassos <patrickpassos93@gmail.com>
2026-03-03 13:00:56 -0300

    conditional to not havea matrix of negative or zero order

--- cada_linha/application/EachLine.java
+++ cada_linha/application/EachLine.java
@@ -9,11 +9,19 @@ public class EachLine {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-
-		System.out.print("What is the order of the matrix? ");
-		int order = sc.nextInt();
-
-		Line matrix = new Line(order);
+		
+		int order = 0;
+		Line matrix = null;
+		
+		while(matrix == null) {
+			System.out.print("What is the order of the matrix? ");
+			order = sc.nextInt();
+			if(order <= 0) {
+				System.out.println("ERROR: There is no order for negative or zero matrices.\n");
+			} else {
+				matrix = new Line(order);
+			}
+		}
 
 		for (int i = 0; i < matrix.getOrder(); i++) {
 			for (int j = 0; j < matrix.getOrder(); j++) {
