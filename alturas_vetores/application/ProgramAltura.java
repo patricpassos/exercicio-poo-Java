caf0ddf92ba19861b8bd9ce69f92f5611fae6e5c
patricpassos <patrickpassos93@gmail.com>
2026-03-07 14:21:23 -0300

    adicionado uma classe para realizar a criação do vetor e os calculos

--- alturas_vetores/application/ProgramAltura.java
+++ alturas_vetores/application/ProgramAltura.java
@@ -3,22 +3,22 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Cadastro;
+//import entities.Cadastro;
 import services.Operacao;
 
 public class ProgramAltura {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("Quantas pessoas serão cadastradas? ");
 		int n = sc.nextInt();
-		
+
 		Operacao vetor = new Operacao(n);
-		
-		for(int i = 0; i < vetor.tamanhoVetor(); i++) {
+
+		for (int i = 0; i < vetor.tamanhoVetor(); i++) {
 			System.out.printf("Dados da %d° pessoa: \n", i + 1);
 			System.out.print("Nome: ");
 			sc.nextLine();
@@ -29,12 +29,21 @@ public class ProgramAltura {
 			double altura = sc.nextDouble();
 			vetor.setElemento(i, nome, idade, altura);
 		}
-		
+
 		double mediaIdade = vetor.mediaAltura();
-		System.out.printf("Altura média = %.2f", mediaIdade);
+		double menores16 = vetor.menores16();
+		String[] nome = vetor.nomesMenor16();
+
+		System.out.printf("\nAltura média = %.2f\n", mediaIdade);
+		System.out.printf(menores16 == 0 ? "Não há menores de 16 anos cadastrado\n" : "Pessoas com menos de 16 anos: %.1f%%\n", menores16);
+		
+		for(String ne : nome) {
+			System.out.println(ne);
+		}
+		
 		
 		sc.close();
-		
+
 	}
 
 }
--- alturas_vetores/services/Operacao.java
+++ alturas_vetores/services/Operacao.java
@@ -3,13 +3,13 @@ package services;
 import entities.Cadastro;
 
 public class Operacao {
-	
+
 	private Cadastro[] vet;
-	
-	public Operacao (int n) {
+
+	public Operacao(int n) {
 		this.vet = new Cadastro[n];
 	}
-	
+
 	public int tamanhoVetor() {
 		return vet.length;
 	}
@@ -17,18 +17,51 @@ public class Operacao {
 	public Cadastro[] getVet() {
 		return vet;
 	}
-	
+
 	public void setElemento(int index, String nome, int idade, double altura) {
 		vet[index] = new Cadastro(nome, idade, altura);
 	}
-	
+
 	public double mediaAltura() {
 		double soma = 0.0;
-		for(Cadastro s : vet) {
+		for (Cadastro s : vet) {
 			soma += s.getAltura();
 		}
-		return soma / vet.length;
+		return soma == 0.0 ? 0.0 : soma / vet.length;
 	}
 
+	public double menores16() {
+		int contMenores = 0;
+		for (Cadastro menores : vet) {
+			if (menores.getIdade() < 16) {
+				contMenores++;
+			}
+		}
+		return (double) contMenores / vet.length * 100.0;
+	}
+	
+	public int contagemMenores16() {
+		int cont = 0;
+		for(Cadastro c : vet) {
+			if(c.getIdade() < 16) {
+				cont++;
+			}
+		}
+		return cont;
+	}
+	
+	public String[] nomesMenor16() {
+		int tamanhoVetor = contagemMenores16();
+		String[] nome = new String[tamanhoVetor];
+		
+		int pos = 0;
+		for(int i = 0; i < vet.length; i++) {
+			if(vet[i].getIdade() < 16) {
+				nome[pos] = vet[i].getNome();
+				pos++;
+			}
+		}
+		return nome;
+	}
 
 }
