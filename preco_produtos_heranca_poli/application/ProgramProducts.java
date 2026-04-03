4aa16fa66fd6cffbaf68a0f61757c6d134bfb836
patricpassos <patrickpassos93@gmail.com>
2026-04-03 13:15:40 -0300

    created list structure and user interface

--- preco_produtos_heranca_poli/application/ProgramProducts.java
+++ preco_produtos_heranca_poli/application/ProgramProducts.java
@@ -2,8 +2,14 @@ package application;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+
+import entities.ImportedProducts;
+import entities.Products;
+import entities.UsedProducts;
 
 public class ProgramProducts {
 
@@ -11,6 +17,8 @@ public class ProgramProducts {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		List<Products> listProducts = new ArrayList<>();
 		
 		System.out.print("Enter the number of products: ");
 		int quantityProducts = sc.nextInt();
@@ -28,13 +36,20 @@ public class ProgramProducts {
 			if(productType == 'u') {
 				System.out.print("Manufacture date (DD/MM/YYYY): ");
 				LocalDate manufactureDate = LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+				Products productUsed = new UsedProducts(name, price, manufactureDate);
+				listProducts.add(productUsed);
 			} else if (productType == 'i') {
 				System.out.print("Customs fee: ");
 				double customsFee = sc.nextDouble();
+				Products productsCustomsFee = new ImportedProducts(name, price, customsFee);
+				listProducts.add(productsCustomsFee);
+			} else {
+				Products productsCommon = new Products(name, price);
+				listProducts.add(productsCommon);
 			}
 		}
 		
-		
+		System.out.println("\nPRICE TAGS:");
 		
 		
 		sc.close();
