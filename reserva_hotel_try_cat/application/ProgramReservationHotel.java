012f9b24a9ff2e1e36aa0e9a49639dabf3188ff6
patricpassos <patrickpassos93@gmail.com>
2026-04-08 16:48:29 -0300

    Exercicio de try-cat e criação de exceções personalizada

--- reserva_hotel_try_cat/application/ProgramReservationHotel.java
+++ reserva_hotel_try_cat/application/ProgramReservationHotel.java
@@ -5,6 +5,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 import model.entities.Reservation;
+import model.exceptions.DomainException;
 
 public class ProgramReservationHotel {
 
@@ -13,23 +14,31 @@ public class ProgramReservationHotel {
 		Scanner sc = new Scanner(System.in);
 		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		
-		System.out.print("Room number: ");
-		int number = sc.nextInt();
-		System.out.print("Check-in date (dd/mm/yyyy): ");
-		LocalDate checkIn = LocalDate.parse(sc.next(), dateFormatter);
-		System.out.print("Check-out date (dd/mm/yyyy): ");
-		LocalDate checkOut = LocalDate.parse(sc.next(), dateFormatter);
-		
-		if(!checkOut.isAfter(checkIn)) {
-			System.out.println("Error in reservation: Check-out date must be after check-in date");
-		} else {
-			System.out.println("Enter data to update the reservation:");
+		try {
+			System.out.print("Room number: ");
+			int number = sc.nextInt();
+			System.out.print("Check-in date (dd/mm/yyyy): ");
+			LocalDate checkIn = LocalDate.parse(sc.next(), dateFormatter);
+			System.out.print("Check-out date (dd/mm/yyyy): ");
+			LocalDate checkOut = LocalDate.parse(sc.next(), dateFormatter);
+			
+			Reservation reservation = new Reservation(number, checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+			
+			System.out.println("\nEnter data to update the reservation:");
 			System.out.print("Check-in date (dd/mm/yyyy): ");
 			checkIn = LocalDate.parse(sc.next(), dateFormatter);
 			System.out.print("Check-out date (dd/mm/yyyy): ");
 			checkOut = LocalDate.parse(sc.next(), dateFormatter);
-			Reservation reservetion = new Reservation(number, checkIn, checkOut);
-			System.out.println("Reservation " + reservetion);
+			
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println("Reservation " + reservation);
+		}
+		catch(DomainException e) {
+			System.out.println("Error in reservation: " + e.getMessage());
+		}
+		catch(RuntimeException e) {
+			System.out.println("Unexpected error");
 		}
 		
 		sc.close();
--- reserva_hotel_try_cat/model/entities/Reservation.java
+++ reserva_hotel_try_cat/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 	
 	private Integer roomNumber;
@@ -14,7 +16,10 @@ public class Reservation {
 		
 	}
 	
-	public Reservation(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) {
+	public Reservation(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) throws DomainException {
+		if(!checkOut.isAfter(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -40,7 +45,14 @@ public class Reservation {
 		return (int) Duration.between(checkIn.atStartOfDay(), checkOut.atStartOfDay()).toDays();
 	}
 
-	public void updateDates(LocalDate checkIn, LocalDate checkOut) {
+	public void updateDates(LocalDate checkIn, LocalDate checkOut) throws DomainException {
+		LocalDate now = LocalDate.now();
+		if(checkIn.isBefore(now) || checkOut.isBefore(now)) {
+			throw new DomainException("Reservation dates for update must be future dates");
+		}
+		if(!checkOut.isAfter(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
 	}
