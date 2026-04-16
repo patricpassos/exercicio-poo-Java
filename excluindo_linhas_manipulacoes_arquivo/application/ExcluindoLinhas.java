b440d3953b755b831c595ad3f6cbc96a130bee94
patricpassos <patrickpassos93@gmail.com>
2026-04-16 17:24:40 -0300

    Excluindo linha, atualizando o arquivo e realizando a leitura

--- excluindo_linhas_manipulacoes_arquivo/application/ExcluindoLinhas.java
+++ excluindo_linhas_manipulacoes_arquivo/application/ExcluindoLinhas.java
@@ -12,39 +12,62 @@ public class ExcluindoLinhas {
 
 	public static void main(String[] args) {
 
-		List<String> list = new ArrayList<>();
+		List<String> listFinal = new ArrayList<>();
 		String path = "c:\\temp\\cadastroBicicletas\\bicicletas.csv";
-		String excluirItem = "optmo";
+		String excluirItem = "impact carbon";
+		String atualizarItemErrado = "arenita";
+		String atualizarItemCorreto = "velox";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String header = br.readLine();
+			if (header != null) {
+				listFinal.add(header);
+			}
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(",");
+				String modeloAtual = fields[1].trim();
+
+				if (modeloAtual.equals(excluirItem)) {
+
+				} else if (modeloAtual.equals(atualizarItemErrado)) {
+					listFinal.add(fields[0] + ", " + atualizarItemCorreto + ", " + fields[2]);
+				} else {
+					listFinal.add(line);
+				}
+				line = br.readLine();
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+			for (String s : listFinal) {
+				bw.write(s);
+				bw.newLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line = br.readLine();
+
 			if (line != null) {
-				list.add(line);
+				String[] header = line.split(",");
+				System.out.printf("\n%-15s %-15s %-15s%n", header[0].trim(), header[1].trim(), header[2].trim());
+				System.out.println("-----------------------------------------------");
 			}
 
 			line = br.readLine();
 			while (line != null) {
 				String[] fields = line.split(",");
-				if (!fields[1].trim().equals(excluirItem)) {
-					list.add(line);
-				}
+				System.out.printf("%-15s %-15s %5s%n", fields[0].trim(), fields[1].trim(),
+						Integer.parseInt(fields[2].trim()));
 				line = br.readLine();
 			}
-
 		} catch (IOException e) {
-			System.out.println("Erro: " + e.getMessage());
-		}
-
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
-			bw.write("marca, modelo, quantidade");
-			bw.newLine();
-			for (String s : list) {
-				bw.write(s);
-				bw.newLine();
-			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println("Error: " + e.getMessage());
 		}
 
 	}
