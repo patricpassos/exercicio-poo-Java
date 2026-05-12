121a68cddac0d67c188a6bacbd034db91f0c6003
patricpassos <patrickpassos93@gmail.com>
2026-05-12 20:06:16 -0300

    Testes exemplo com o Predicate

--- interface_predicate/application/ProgramPredicateProdutos.java
+++ interface_predicate/application/ProgramPredicateProdutos.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import entities.PredicateProdutos;
 
@@ -24,14 +25,24 @@ public class ProgramPredicateProdutos {
 		//list.removeIf(PredicateProdutos::staticProdutoPredicate); //Referência do metodo estatico
 		//list.removeIf(PredicateProdutos::nonStaticProdutoPredicate); //Referência do metodo não estatico
 		
-		double min = 100.0;
+		Predicate<PredicateProdutos> isCheap = p -> p.getPreco() < 100.0;
+		Predicate<PredicateProdutos> startsWithT = p -> p.getNome().startsWith("T");
+		List<PredicateProdutos> onlyT = list.stream().filter(startsWithT).collect(Collectors.toList());
+		
+		System.out.println("Produtos que começam com 'T':");
+        onlyT.forEach(System.out::println);
+        
+        boolean existsCheap = list.stream().anyMatch(isCheap); 
+        System.out.println("\nExiste algum produto barato (< 100)? " + existsCheap);
+        
+        System.out.println("\nProdutos baratos E que começam com 'T':");
+        list.stream().filter(isCheap.and(startsWithT)).forEach(System.out::println);
+        
+        System.out.println();
+        double min = 100.0;
 		Predicate<PredicateProdutos> pred = p -> p.getPreco() >= min; // Expressão lambda declarada pode-se usar variaveis de entrada
 		list.removeIf(pred);
-		
 		list.forEach(System.out::println);
-		//for(PredicateProdutos p : list) {
-		//	System.out.println(p);
-		//}
 
 	}
 
